7dff922cad243cf341729792845e3ce2d3f8fa52
KingdomRoses <mikaela.seabourne@utas.edu.au>
2025-06-17 08:53:46 +0000

    learningssl

--- index.php
+++ index.php
@@ -1,64 +1,39 @@
 <?php
-// ...existing code...
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-// ...existing code...
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Process form data here
-
-    header('Location: success.php');
-    exit;
-}
 
 if(isset($_POST['name'])){
-
-
-
 //set connection variables
 $conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, 'reboot-server.mysql.database.azure.com', 'skmnhokqqk', 'U44$dIg6$TVuoW53', 'reboot-server', 3306, MYSQLI_CLIENT_SSL);
+
+//enable SSL *current point of concern*
+mysqli_ssl_set($conn,NULL,NULL, __DIR__."/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+
+//connection to database
+mysqli_real_connect($conn, 'reboot-server.mysql.database.azure.com', 'skmnhokqqk', '10 things I hate about you!', 'reboot-database', 3306, MYSQLI_CLIENT_SSL);
 if (mysqli_connect_errno($conn)) {
-//die('Failed to connect to MySQL: '.mysqli_connect_error()
-header(location: "/failure.php");
+die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
+$name = 'john'; // For testing purposes, you can replace this with $_POST['name'] when ready
 
 
+$sql = "INSERT INTO `login` (`name`) VALUES ('$name');";
+echo $sql;
 
-// ...existing code...
-$result = $conn->query("SELECT * FROM trip.trip");
+$result = $conn->query("SELECT * FROM login");
 while ($row = $result->fetch_assoc()) {
     echo htmlspecialchars($row['name']) . "<br>";
 }
-// ...existing code...
-
-//collect post variables
-$name=$_POST['name'];
-$email=$_POST['email'];
-$gender=$_POST['gender'];
-$age=$_POST['age'];
-$phone=$_POST['phone'];
-$desc=$_POST['desc'];
-
-
-$sql="INSERT INTO `trip`.`trip` (`name`, `email`, `gender`, `age`, `phone`, `other`,
- `date`) VALUES ('$name', '$email', '$gender', '$age','$phone',' $desc', '2023-01-07 15:51:10.000000');";
-//echo $sql;
-
 
 
 //execute the query
 if($conn->query($sql)==true)
 {
     //echo "Successfully inserted";
-    header(location: "/success.php");
+    header("Location: /success.php");
    
 }
 else{
     //echo "ERROR $sql <br> $conn->error"; 
-    header(location: "/failure.php");
+    header("Location: /failure.php");
 }
 
 
@@ -76,7 +51,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to Travel form</title>
+    <title>Welcome to the rebootCTF!</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
@@ -84,16 +59,11 @@ $conn->close();
     
 <img class="bg" src="bag.jpg" alt="KKWIEER">
 <div class="container"> 
-    <h1>Welcome to KKWIEER! Gujrat Trip Form</h1>
-    <p> Enter details to register</p>
+    <h1>A totally legitimate website...</h1>
+    <p> Enter your name to access the challenges!</p>
 
     <form action="index.php" method="post">
         <input type="text" name="name" id="name" placeholder="Enter your name">
-        <input type="text" name="email" id="email" placeholder="Enter your email-id">
-        <input type="text" name="gender" id="gender" placeholder="Enter your gender">
-        <input type="text" name="age" id="age" placeholder="Enter your age">
-        <input type="text" name="contact" id="contact" placeholder="Enter your contact number">
-        <textarea name="desc" id="desc" cols="30" rows="10" placeholder="Enter any other information "></textarea>
         <button class="btn">Submit</button>
     
     
