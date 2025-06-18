886545ba5a7fe1f2401151f7eadf853cee796bcc
KingdomRoses <mikaela.seabourne@utas.edu.au>
2025-06-19 09:25:00 +1000

    Update index.php
    database connection redo using old school no TLS :D

--- index.php
+++ index.php
@@ -1,42 +1,23 @@
 <?php
 
-if(isset($_POST['name'])){
-//set connection variables
-$conn = mysqli_init();
+//set universal variables
+$servername = "reboot-server.mysql.database.azure.com";
+$username = "skmnhokqqk";
+$password = "10 things I hate about you!";
+$database = "players";
+$table = "competitors";
 
-//enable SSL *current point of concern*
-mysqli_ssl_set($conn,NULL,NULL, __DIR__."/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+//create a new connection using universal variables
+$conn = new mysqli($servername, $username, $password);
 
-//connection to database
-mysqli_real_connect($conn, 'reboot-server.mysql.database.azure.com', 'skmnhokqqk', '10 things I hate about you!', 'reboot-database', 3306, MYSQLI_CLIENT_SSL);
+//if connection fails, die and echo error
 if (mysqli_connect_errno($conn)) {
-die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-$name = 'john'; // For testing purposes, you can replace this with $_POST['name'] when ready
-
-
-$sql = "INSERT INTO `login` (`name`) VALUES ('$name');";
-echo $sql;
-
-$result = $conn->query("SELECT * FROM login");
-while ($row = $result->fetch_assoc()) {
-    echo htmlspecialchars($row['name']) . "<br>";
+    die("Failed to connect to MySQL: " . $conn->connect_error);
 }
 
-
-//execute the query
-if($conn->query($sql)==true)
-{
-    //echo "Successfully inserted";
-    header("Location: /success.php");
-   
-}
-else{
-    //echo "ERROR $sql <br> $conn->error"; 
-    header("Location: /failure.php");
-}
-
-
+//if the connection succeeds echo confirmation
+echo "Connected Successfully"
+    
 //close db connection
 $conn->close();
 }
@@ -45,33 +26,3 @@ $conn->close();
 
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to the rebootCTF!</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-
-<body>
-    
-<img class="bg" src="bag.jpg" alt="KKWIEER">
-<div class="container"> 
-    <h1>A totally legitimate website...</h1>
-    <p> Enter your name to access the challenges!</p>
-
-    <form action="index.php" method="post">
-        <input type="text" name="name" id="name" placeholder="Enter your name">
-        <button class="btn">Submit</button>
-    
-    
-    </form>
-
-
-</div>
-
-
-</body>
-</html>
